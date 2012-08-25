c4e8d380a64c97d6e1be7446d4f70bf957fb4722
Dustin Sallings <dustin@spy.net>
2012-08-25 13:21:43 -0700

    Somewhat working.

---
+++ couchstore.go
@@ -0,0 +1,168 @@
+// Couchstore API for go.
+package couchstore
+
+/*
+#cgo LDFLAGS: -lcouchstore
+
+#include <errno.h>
+#include <string.h>
+#include <sysexits.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <libcouchstore/couch_db.h>
+
+static void initDocInfo(DocInfo *info) {
+ memset(info, 0x00, sizeof(*info));
+}
+*/
+import "C"
+
+import (
+	"unsafe"
+)
+
+type Couchstore struct {
+	db     *C.Db
+	isOpen bool
+}
+
+type couchError int
+
+type Document struct {
+	doc C.Doc
+}
+
+type DocInfo struct {
+	info C.DocInfo
+}
+
+func (e couchError) Error() string {
+	return C.GoString(C.couchstore_strerror(_Ctype_couchstore_error_t(e)))
+}
+
+func maybeError(e _Ctype_couchstore_error_t) error {
+	if e != C.COUCHSTORE_SUCCESS {
+		return couchError(e)
+	}
+	return nil
+}
+
+// Open a database.
+func Open(pathname string, create bool) (*Couchstore, error) {
+	rv := &Couchstore{}
+	flags := _Ctype_couchstore_open_flags(0)
+	if create {
+		flags = C.COUCHSTORE_OPEN_FLAG_CREATE
+	}
+	err := maybeError(C.couchstore_open_db(C.CString(pathname),
+		flags, &rv.db))
+	if err == nil {
+		rv.isOpen = true
+	} else {
+		rv = nil
+	}
+
+	return rv, err
+}
+
+// Close the database.
+func (db *Couchstore) Close() error {
+	if db == nil || !db.isOpen {
+		return nil
+	}
+
+	return maybeError(C.couchstore_close_db(db.db))
+}
+
+// Commit pending data.
+func (db *Couchstore) Commit() error {
+	return maybeError(C.couchstore_commit(db.db))
+}
+
+// Store a document.
+func (db *Couchstore) Save(doc Document, docInfo DocInfo) error {
+	return maybeError(C.couchstore_save_document(db.db,
+		&doc.doc, &docInfo.info, C.COMPRESS_DOC_BODIES))
+}
+
+// Get a new document instance with the given id and value.
+func NewDocument(id, value string) Document {
+	doc := Document{}
+
+	doc.doc.id.buf = C.CString(id)
+	doc.doc.id.size = _Ctype_size_t(len(id))
+	doc.doc.data.buf = C.CString(value)
+	doc.doc.data.size = _Ctype_size_t(len(value))
+
+	return doc
+}
+
+// Get the ID of this document
+func (doc Document) ID() string {
+	return C.GoStringN(doc.doc.id.buf, _Ctype_int(doc.doc.id.size))
+}
+
+// Get the value of this document.
+func (doc Document) Value() string {
+	return C.GoStringN(doc.doc.data.buf, _Ctype_int(doc.doc.data.size))
+}
+
+// Create a new docinfo.
+func NewDocInfo(id string, meta uint8) DocInfo {
+	info := DocInfo{}
+	C.initDocInfo(&info.info)
+
+	info.info.id.buf = C.CString(id)
+	info.info.id.size = _Ctype_size_t(len(id))
+
+	info.info.content_meta = _Ctype_couchstore_content_meta_flags(meta)
+
+	return info
+}
+
+// Get the ID of this document info
+func (info DocInfo) ID() string {
+	return C.GoStringN(info.info.id.buf, _Ctype_int(info.info.id.size))
+}
+
+func (db *Couchstore) getDocInfo(id string) (DocInfo, error) {
+	var inf *C.DocInfo
+	err := maybeError(C.couchstore_docinfo_by_id(db.db,
+		unsafe.Pointer(C.CString(id)), _Ctype_size_t(len(id)), &inf))
+	if err == nil {
+		return DocInfo{*inf}, nil
+	}
+	return DocInfo{}, err
+}
+
+func (db *Couchstore) getFromDocInfo(info DocInfo) (Document, error) {
+	var doc *C.Doc
+	rv := Document{}
+
+	err := maybeError(C.couchstore_open_doc_with_docinfo(db.db,
+		&info.info, &doc, 0))
+	if err == nil {
+		rv.doc = *doc
+	}
+	return rv, err
+}
+
+// Retrieve a document.
+func (db *Couchstore) Get(id string) (Document, DocInfo, error) {
+	di, err := db.getDocInfo(id)
+	if err != nil {
+		return Document{}, di, err
+	}
+
+	doc, err := db.getFromDocInfo(di)
+
+	return doc, di, err
+}
+
+// Delete a document.
+func (db *Couchstore) Delete(id string) error {
+	di := NewDocInfo(id, 0)
+	di.info.deleted = 1
+	return db.Save(NewDocument(id, ""), di)
+}
---
+++ couchstore_test.go
@@ -0,0 +1,71 @@
+package couchstore
+
+import (
+	"os"
+	"testing"
+)
+
+const testFilename = ",test-database.couch"
+
+func TestOpenFailure(t *testing.T) {
+	db, err := Open(testFilename, false)
+	if err == nil {
+		t.Fatalf("Expected error opening non-existent database")
+	}
+	if db != nil {
+		t.Fatalf("Got a database!")
+	}
+}
+
+func TestOpenSuccess(t *testing.T) {
+	db, err := Open(",test-database.couch", true)
+	if err != nil {
+		t.Fatalf("Error creating database:  %v", err)
+	}
+	defer db.Close()
+	defer os.Remove(testFilename)
+}
+
+func TestDocumentMutation(t *testing.T) {
+	db, err := Open(",test-database.couch", true)
+	if err != nil {
+		t.Fatalf("Error creating database:  %v", err)
+	}
+	defer db.Close()
+	defer os.Remove(testFilename)
+
+	err = db.Save(NewDocument("x", "value of x"), NewDocInfo("x", 0))
+	if err != nil {
+		t.Fatalf("Error saving new document:  %v", err)
+	}
+
+	if err = db.Commit(); err != nil {
+		t.Fatalf("Error committing change: %v", err)
+	}
+
+	doc, di, err := db.Get("x")
+	if err != nil {
+		t.Fatalf("Error loading stored document: %v", err)
+	}
+
+	if di.ID() != "x" {
+		t.Fatalf("Expected info id 'x', got %#v", di.ID())
+	}
+	if doc.Value() != "value of x" {
+		t.Fatalf("Expected doc value 'value of x', got %#v", doc)
+	}
+	if doc.ID() != "x" {
+		t.Fatalf("Expected doc id 'x', got %#v", doc)
+	}
+
+	err = db.Delete("x")
+	if err != nil {
+		t.Fatalf("Error deleting document: %v", err)
+	}
+
+	doc2, di2, err := db.Get("x")
+	if err != nil {
+		t.Fatalf("Expected error getting deleted doc, got %#v/%#v",
+			di2, doc2)
+	}
+}
